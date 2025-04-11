eaaadfd47737a0330ec2f99beb1de657f77f4986
redney757 <redney@maets.net>
2025-04-10 21:39:33 -0400

    finished navigation

--- javascript.js
+++ javascript.js
@@ -1,16 +1,27 @@
 const navIcon = document.getElementById('nav-icon4');
 const rightNav = document.getElementById('rightNav');
-
+const mobileNav = document.getElementById('mobileNavList');
 // Store original margin
 const originalMargin = getComputedStyle(navIcon).marginRight;
 
 navIcon.addEventListener('click', () => {
     navIcon.classList.toggle('open');
     rightNav.classList.toggle('open');
-
+    mobileNav.classList.toggle('open');
     if (rightNav.classList.contains('open')) {
         navIcon.style.marginRight = '250px';
     } else {
         navIcon.style.marginRight = originalMargin;
+        
     }
 });
+
+window.addEventListener('click', () => {
+    if (rightNav.classList.contains('open') && !navIcon.contains(event.target) && !rightNav.contains(event.target)) {
+        navIcon.classList.remove('open');
+        rightNav.classList.remove('open');
+        mobileNav.classList.remove('open');
+        navIcon.style.marginRight = originalMargin;
+    }
+}
+)
