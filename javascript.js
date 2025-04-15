0f643634d7afae4689e47ebc2e9149911519f4c7
Roland Edney MAETS <redney@maets.net>
2025-04-15 15:20:26 -0400

    more styling on about page

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
 const navIcon = document.getElementById('nav-icon4');
 const rightNav = document.getElementById('rightNav');
 const mobileNav = document.getElementById('mobileNavList');
+const clickShowLabel = document.getElementById('clickShowLabel');
+const clickReset = document.getElementById('clickReset');
+const clickShow = document.getElementById('clickShow');
+
 // Store original margin
 const originalMargin = getComputedStyle(navIcon).marginRight;
 console.log("Hello world")
@@ -24,3 +28,48 @@ window.addEventListener('click', () => {
     }
 }
 )
+
+clickShowLabel.addEventListener('click', () =>{
+    const p1Text = document.getElementById('p1Text')
+    if (p1Text.style.opacity ==  0) {
+        clickShowLabel.style.display = 'none';
+        p1Text.style.opacity = 1;
+        clickReset.style.display = 'block';
+
+    }
+    
+
+})
+clickReset.addEventListener('click', () =>{
+    const p1Text = document.getElementById('p1Text')
+    if (p1Text.style.opacity ==  1) {
+        clickShowLabel.style.display = 'block';
+        p1Text.style.opacity = 0;
+        clickReset.style.display = 'none';
+        
+    }
+    
+
+})
+clickReset.addEventListener('mouseover', () =>{
+    clickReset.style.transform = 'scale(0.9)';
+    clickReset.style.transition = 'all 0.2s ease'
+
+})
+clickReset.addEventListener('mouseout', () =>{
+    clickReset.style.transform = 'scale(1)';
+    clickReset.style.transition = 'all 0.2s ease'
+
+
+})
+clickShow.addEventListener('mouseover', () =>{
+    clickShow.style.transform = 'scale(0.9)';
+    clickShow.style.transition = 'all 0.2s ease'
+
+})
+clickShow.addEventListener('mouseout', () =>{
+    clickShow.style.transform = 'scale(1)';
+    clickShow.style.transition = 'all 0.2s ease'
+
+
+})
