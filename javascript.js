ca83c7eb984bfef585a00e22b5c2df0bb6a4d567
redney757 <redney@maets.net>
2025-04-17 20:55:51 -0400

    added some pseudo code

--- javascript.js
+++ javascript.js
@@ -1,5 +1,8 @@
+//adding a console.log to show up on everypage that has the <script> tag and references this file
 console.log('hello world')
+//this line of javascript adds a "listener" to the page that is loading the script. This part of the script is only ran once the page loads. I had to lay it out like this because the way i had it set up originally woudld cause elements not to work, reason being, javascript would try and run something but those elements would not be found.
 document.addEventListener("DOMContentLoaded", () => {
+    //This is adding all the variables for the if statements in this section of javascript and reference elements from various pages
     const navIcon = document.getElementById('nav-icon4');
     const rightNav = document.getElementById('rightNav');
     const mobileNav = document.getElementById('mobileNavList');
@@ -13,18 +16,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const instagram = document.getElementById('instagram');
     const facebook = document.getElementById('facebook');
     const twitter = document.getElementById('twitter');
-    const originalMargin = navIcon ? getComputedStyle(navIcon).marginRight : "0px";
+    const originalMargin = navIcon ? getComputedStyle(navIcon).marginRight : "0px"; // this is to get how an element is currently styled with a specific attribute.
     const originalPadding = clickShow ? getComputedStyle(clickShow).padding : "15px";
-    const successLocation = window.location.origin + '/success.html';
-
+    const successLocation = window.location.origin + '/success.html'; // this gets the url of the origin / loading page which is essentially localhost/ when making a website live, whatever you are using to host your website uses the index.html, so it does not need to be explicitly specified. With this logic we can safely find the origin url with this code and add what page we are trying to find with the string '' in this case mine was /success.html because i wanted to find the origin and add that at the end of it.
+    // all if statements in this section at the top level check to see if these elements exist on the page first, if they do then the next part of the if can be ran
     if (navIcon && rightNav && mobileNav) {
         navIcon.addEventListener('click', () => {
             navIcon.classList.toggle('open');
             rightNav.classList.toggle('open');
             mobileNav.classList.toggle('open');
+            //this line is essentially setting an if statement without an if statement explicitly defined. so the navIcon's margin is set equal to ( if the rightnav.classlist contains the class name open) the ? is the (if else) so if it does contain open class then the margin will be 250px otherwise the margin will be set to its original margin.
             navIcon.style.marginRight = rightNav.classList.contains('open') ? '250px' : originalMargin;
         });
-
+        //this adds an event listener on any window that is currently loaded and listens for the 'click' event. This then passes the event attribute to the elements on the page and listens for a click, but the javascript is only checking if the rightNav contains the open class and the ! declaration ensures that the click did not happen on these elements in this case the navicon or the element itself and if all conditions are true it means the user clicked somewhere outside
         window.addEventListener('click', (event) => {
             if (rightNav.classList.contains('open') &&
                 !navIcon.contains(event.target) &&
